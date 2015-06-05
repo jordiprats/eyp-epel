0c42c802ab32a3380b22968a7e98b03525da46ff
Jordi Prats <jprats@systemadmin.es>
2015-06-05 16:55:35 +0200

    arreglat estil

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,19 +35,16 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class epel() inherits epel::params {
+class epel () inherits epel::params {
+  exec { 'update-ca':
+    command => '/usr/bin/yum upgrade ca-certificates --disablerepo=epel* -y > /root/yum.ca-certificates.log',
+    creates => '/root/yum.ca-certificates.log',
+  }
 
-	exec { 'update-ca':
-		command => '/usr/bin/yum upgrade ca-certificates --disablerepo=epel* -y > /root/yum.ca-certificates.log',
-		creates => '/root/yum.ca-certificates.log',		
-	}
-	
-	package { 'epel-release':
-		ensure => installed,
-		provider => $epel::params::rpmprovider,
-		source => $epel::params::sourcerpm,
-		require => Exec['update-ca'],
-	}
-
-	#epel-release
+  package { 'epel-release':
+    ensure   => installed,
+    provider => $epel::params::rpmprovider,
+    source   => $epel::params::sourcerpm,
+    require  => Exec['update-ca'],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,30 +1,27 @@
 class epel::params {
+  case $::osfamily {
+    'redhat' : {
+      case $::operatingsystemrelease {
+        /^5.*$/ : {
+          $rpmprovider = 'rpm'
+          $sourcerpm = 'http://mirror.uv.es/mirror/fedora-epel/5/x86_64/epel-release-5-4.noarch.rpm'
+        }
+        /^6.*$/ : {
+          $rpmprovider = 'rpm'
+          $sourcerpm = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+        }
+        /^7.*$/ : {
+          $rpmprovider = 'yum'
+          $sourcerpm = undef
+        }
+        default : {
+          fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")
+        }
+      }
 
-	case $::osfamily 
-	{
-		'redhat':
-    		{
-			case $::operatingsystemrelease 
-			{
-				/^5.*$/:
-					{
-						$rpmprovider='rpm'
-						$sourcerpm='http://mirror.uv.es/mirror/fedora-epel/5/x86_64/epel-release-5-4.noarch.rpm'
-					}
-				/^6.*$/:
-					{
-						$rpmprovider='rpm'
-						$sourcerpm='http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
-					}
-				/^7.*$/: 
-					{
-						$rpmprovider='yum'
-						$sourcerpm=undef
-					}
-				default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
-			}
-
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+    }
+    default  : {
+      fail("Unsupported OS!")
+    }
+  }
 }
