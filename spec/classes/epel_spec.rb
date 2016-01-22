a024c0e469e2118d15539710926452590feac788
Jordi Prats <jprats@systemadmin.es>
2016-01-22 13:33:04 +0100

    estil rspec test

--- spec/classes/epel_spec.rb
+++ spec/classes/epel_spec.rb
@@ -2,54 +2,54 @@ require 'spec_helper'
 
 describe 'epel', :type => 'class' do
 
-context "RH 5" do
-	let :facts do
-	{
-		:osfamily => 'RedHat',
-		:operatingsystemrelease => '5.1'
-	}
-	end
+  context "RH 5" do
+    let :facts do
+    {
+      :osfamily => 'RedHat',
+      :operatingsystemrelease => '5.1'
+    }
+    end
 
-	it {
-		should contain_package('epel-release')
-	}
-	end
+    it {
+      should contain_package('epel-release')
+    }
+  end
 
-context "RH 6" do
-	let :facts do
-	{
-	        :osfamily => 'RedHat',
-	        :operatingsystemrelease => '6.1'
-	}
-	end
+  context "RH 6" do
+    let :facts do
+    {
+      :osfamily => 'RedHat',
+      :operatingsystemrelease => '6.1'
+    }
+    end
 
-	it {
-	        should contain_package('epel-release')
-	}
-end
+    it {
+      should contain_package('epel-release')
+    }
+  end
 
-context "RH 7" do
-        let :facts do
-        {
-                :osfamily => 'RedHat',
-                :operatingsystemrelease => '7.1'
-        }
-        end
+  context "RH 7" do
+    let :facts do
+    {
+      :osfamily => 'RedHat',
+      :operatingsystemrelease => '7.1'
+    }
+    end
 
-        it {
-                should contain_package('epel-release')
-        }
-        end
+    it {
+      should contain_package('epel-release')
+    }
+  end
 
-context "unsupported OS" do
-        let :facts do
-        {
-                :osfamily => 'Darwin',
-        }
-        end
+  context "unsupported OS" do
+    let :facts do
+    {
+      :osfamily => 'Darwin',
+    }
+    end
 
-        it {
-                expect { should raise_error(Puppet::Error) }
-        }
-        end
+    it {
+      expect { should raise_error(Puppet::Error) }
+    }
+  end
 end
