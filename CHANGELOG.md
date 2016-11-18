c9bb5c7ff46296a31770ae79ee5f88da5d1baecb
Jordi Prats <jprats@systemadmin.es>
2016-11-18 15:50:16 +0100

    afegit schedule

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
 # CHANGELOG
 
+## 3.1.18
+
+* yum check-update executed only once a day on puppet version >= 3.8
--- manifests/init.pp
+++ manifests/init.pp
@@ -8,11 +8,28 @@ class epel ($ensure='installed', $manage_ca_certificates=true) inherits epel::pa
 
   if($manage_ca_certificates)
   {
-    #yum check-update
-    exec { 'update-ca':
-      command => '/usr/bin/yum upgrade ca-certificates --disablerepo =epel* -y > /var/log/yum.ca-certificates.log',
-      unless  => '/usr/bin/yum check-update ca-certificates',
-      before  => Package['epel-release'],
+    if versioncmp($::puppetversion, '3.8.0') < 0
+    {
+      #yum check-update
+      exec { 'update-ca':
+        command => '/usr/bin/yum upgrade ca-certificates --disablerepo =epel* -y',
+        unless  => '/usr/bin/yum check-update ca-certificates',
+        before  => Package['epel-release'],
+      }
+    }
+    else
+    {
+      schedule { 'eyp-epel daily yum check-update':
+        period => daily,
+        repeat => 1,
+      }
+
+      exec { 'update-ca':
+        command  => '/usr/bin/yum upgrade ca-certificates --disablerepo =epel* -y',
+        unless   => '/usr/bin/yum check-update ca-certificates',
+        before   => Package['epel-release'],
+        schedule => 'eyp-epel daily yum check-update',
+      }
     }
   }
 
