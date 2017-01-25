94bbb0949682d9cc60940922169d7e729c6df5a1
Jordi Prats <jprats@systemadmin.es>
2017-01-25 12:38:02 +0100

    estil

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,31 +1,42 @@
-class epel::params {
-  case $::osfamily {
-    'redhat' : {
-      case $::operatingsystem {
+class epel::params
+{
+  case $::osfamily
+  {
+    'redhat' :
+    {
+      case $::operatingsystem
+      {
           'Fedora': { notice ("EPEL will not be installed on ${::operatingsystem} ${::operatingsystemrelease}") }
           'Amazon': { notice ("EPEL will not be managed on ${::operatingsystem} ${::operatingsystemrelease}") }
-          default: {
-            case $::operatingsystemrelease {
-              /^5.*$/ : {
+          default:
+          {
+            case $::operatingsystemrelease
+            {
+              /^5.*$/ :
+              {
                 $rpmprovider = 'rpm'
                 $sourcerpm = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm'
               }
-              /^6.*$/ : {
+              /^6.*$/ :
+              {
                 $rpmprovider = 'rpm'
                 $sourcerpm = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm'
               }
-              /^7.*$/ : {
+              /^7.*$/ :
+              {
                 $rpmprovider = 'rpm'
                 $sourcerpm = 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
               }
-              default : {
+              default :
+              {
                 fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")
               }
             }
           }
       }
     }
-    default  : {
+    default  :
+    {
       notice ("${::operatingsystem} will not have the EPEL repository applied")
     }
   }
