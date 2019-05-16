cb3f952c2a3105000bf30ed1d1ac3463b2695b95
Jordi Prats <jordi.prats@gmail.com>
2019-05-16 12:26:57 +0200

    lint

--- manifests/import_gpg_key.pp
+++ manifests/import_gpg_key.pp
@@ -4,8 +4,8 @@ define epel::import_gpg_key (
                             ) {
 
   exec {  "GPG import ${key_name}":
-    path      => '/bin:/usr/bin:/sbin:/usr/sbin',
-    command   => "rpm --import ${path}",
-    unless    => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids --keyid-format short < ${path}) | cut --characters=11-18 | tr '[A-Z]' '[a-z]')",
+    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+    command => "rpm --import ${path}",
+    unless  => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids --keyid-format short < ${path}) | cut --characters=11-18 | tr '[A-Z]' '[a-z]')",
   }
 }
